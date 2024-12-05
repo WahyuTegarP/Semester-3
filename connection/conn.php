b639a0cd216462fef709b02b0492aafeea5d75d7
nylaaptr <naylawardhani488@gmail.com>
2024-12-06 00:32:19 +0700

    mengubah koneksi

--- connection/conn.php
+++ connection/conn.php
@@ -6,5 +6,7 @@ $db = "db_sewakamera";
 $conn = mysqli_connect($server, $username, $password, $db);
 if (mysqli_connect_errno()){
     echo "koneksi gagal:".mysqli_connect_error();
+}else{
+    echo "koneksi berhasil";
 }
 ?>
